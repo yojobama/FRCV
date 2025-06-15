2780058ff482a442285c49cc67e144b742e44538
John <yojobama@gmail.com>
2025-06-15 13:27:50 +0300

    fixing a problem in the unit test base class

--- FRCV/UnitTests/UnitTestBase.cpp
+++ FRCV/UnitTests/UnitTestBase.cpp
@@ -12,7 +12,7 @@ UnitTestBase::~UnitTestBase()
 
 void UnitTestBase::doTest()
 {
-	if (!innerTest) {
+	if (!this->innerTest()) { // Call the innerTest function
 		std::vector<Log> logs = logger->GetAllLogs();
 		for (int i = 0; i < logs.size(); i++) {
 			std::cout << logs.at(i).GetMessage() << std::endl;
