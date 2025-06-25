47bf8ae7ba08106e9faae8d394c9cd8f97a51ef5
John <yojobama@gmail.com>
2025-06-25 13:24:23 +0300

    making some changes

--- FRCV/UnitTests/UnitTestBase.cpp
+++ FRCV/UnitTests/UnitTestBase.cpp
@@ -13,9 +13,9 @@ UnitTestBase::~UnitTestBase()
 void UnitTestBase::doTest()
 {
 	if (!this->innerTest()) { // Call the innerTest function
-		std::vector<Log> logs = logger->GetAllLogs();
-		for (int i = 0; i < logs.size(); i++) {
-			std::cout << logs.at(i).GetMessage() << std::endl;
+		std::vector<Log*>* logs = logger->GetAllLogs();
+		for (int i = 0; i < logs->size(); i++) {
+			std::cout << logs->at(i)->GetMessage() << std::endl;
 		}
 	}
 }
