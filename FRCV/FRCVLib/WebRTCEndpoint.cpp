116342513a379ed46d11d15fc6ce0f4d18f7589f
Yoav John Barak-Maurice <yojobama@gmail.com>
2025-11-16 23:33:29 +0200

    implementing the frame rendering method as well as the base of the ProcessFrame function in the WebRTCEndpoint class (also adding a framepool)

--- FRCV/FRCVLib/WebRTCEndpoint.cpp
+++ FRCV/FRCVLib/WebRTCEndpoint.cpp
@@ -1,1 +1,38 @@
 #include "WebRTCEndpoint.h"
+
+#include "FilterAnalysis.h"
+#include "Frame.h"
+#include "DrawCommands.h"
+#include "FramePool.h"
+
+WebRTCEndpoint::WebRTCEndpoint(std::shared_ptr<Logger> logger, std::shared_ptr<FramePool> framePool) : EndpointBase(logger)
+{
+	m_FramePool = framePool;
+}
+
+WebRTCEndpoint::~WebRTCEndpoint()
+{
+}
+
+void WebRTCEndpoint::ProcessFrame(std::vector<FilterAnalysis> analysisVector)
+{
+	std::shared_ptr<Frame> frame = m_FramePool->GetFrame(FrameSpec());
+	RenderFrameWithAnalysisVector(frame, analysisVector);
+	// Here you would typically send the frame via WebRTC
+	m_FramePool->ReturnFrame(frame);
+}
+
+void WebRTCEndpoint::InitializePeerConnection()
+{
+}
+
+void WebRTCEndpoint::RenderFrameWithAnalysisVector(std::shared_ptr<Frame> frame, std::vector<FilterAnalysis> analysisVector)
+{
+	for (FilterAnalysis analysis : analysisVector)
+	{
+		for (std::shared_ptr<DrawCommands::DrawCommandBase> command : analysis.GetDrawCommands()) 
+		{
+			command->Execute(frame);
+		}
+	}
+}
--- FRCV/FRCVLib/WebRTCEndpoint.h
+++ FRCV/FRCVLib/WebRTCEndpoint.h
@@ -2,21 +2,24 @@
 #include "EndpointBase.h"
 #include <memory>
 #include <rtc/rtc.hpp>
+#include <vector>
 
+class FramePool;
 class Frame;
 
 class WebRTCEndpoint : public EndpointBase
 {
 public:
-	WebRTCEndpoint(std::shared_ptr<Logger> logger);
+	WebRTCEndpoint(std::shared_ptr<Logger> logger, std::shared_ptr<FramePool> framePool);
 	~WebRTCEndpoint();
 
 	void ProcessFrame(std::vector<FilterAnalysis> analysisVector) override;
 private:
 	void InitializePeerConnection();
 
-	void RenderFrameWithAnalysis(std::shared_ptr<Frame> frame, std::shared_ptr<FilterAnalysis> analysis);
+	void RenderFrameWithAnalysisVector(std::shared_ptr<Frame> frame, std::vector<FilterAnalysis> analysisVector);
 
 	std::shared_ptr<rtc::PeerConnection> m_PeerConnection;
+	std::shared_ptr<FramePool> m_FramePool;
 };
 
