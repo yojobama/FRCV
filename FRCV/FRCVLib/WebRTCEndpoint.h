2df280caa852c0c7d1a95d2278f2b34c7c2d4681
Yoav John Barak-Maurice <yojobama@gmail.com>
2025-11-16 22:52:37 +0200

    adding the base of the WebRTCEndpoint and linking the WebRTC library to the project

--- FRCV/FRCVLib/WebRTCEndpoint.h
+++ FRCV/FRCVLib/WebRTCEndpoint.h
@@ -1,5 +1,22 @@
 #pragma once
-class WebRTCEndpoint
+#include "EndpointBase.h"
+#include <memory>
+#include <rtc/rtc.hpp>
+
+class Frame;
+
+class WebRTCEndpoint : public EndpointBase
 {
+public:
+	WebRTCEndpoint(std::shared_ptr<Logger> logger);
+	~WebRTCEndpoint();
+
+	void ProcessFrame(std::vector<FilterAnalysis> analysisVector) override;
+private:
+	void InitializePeerConnection();
+
+	void RenderFrameWithAnalysis(std::shared_ptr<Frame> frame, std::shared_ptr<FilterAnalysis> analysis);
+
+	std::shared_ptr<rtc::PeerConnection> m_PeerConnection;
 };
 
