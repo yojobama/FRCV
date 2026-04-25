732c435e8ca07ea1773044a70a9f349e005dfd56
John <yojobama@gmail.com>
2026-04-25 18:27:18 +0300

    adding an extremely basic version of the VideoFileRecorder

---
+++ FRCV/FRCVLib/VideoFileRecorder.cpp
@@ -0,0 +1,15 @@
+#include "VideoFileRecorder.h"
+
+VideoFileRecorder::VideoFileRecorder(Logger* logger, const std::string& dst, const std::string& id, int height, int width, int fps) : ISink(logger, 1, false, true, id)
+{
+	m_Dst = dst;
+	m_VideoWriter = cv::VideoWriter(dst, cv::VideoWriter::fourcc('x', '2', '6', '4'), fps, cv::Size(width, height)); // Todo: check if the fourcc code is correct
+}
+
+void VideoFileRecorder::Process(std::vector<SourceResult> sources)
+{
+	SourceResult& source = sources.front();
+	if (source.frame.has_value()) {
+		m_VideoWriter << *source.frame.value();
+	}
+}
---
+++ FRCV/FRCVLib/VideoFileRecorder.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include <opencv2/opencv.hpp>
+
+#include "ISink.h"
+
+class VideoFileRecorder : ISink
+{
+public:
+	VideoFileRecorder(Logger* logger, const std::string& dst, const std::string& id, int height, int width, int fps);
+private:
+	void Process(std::vector<SourceResult> sources) override;
+
+	cv::VideoWriter m_VideoWriter;
+	std::string m_Dst;
+	std::shared_ptr<ISource> m_Source;
+};
+
